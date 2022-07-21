cb595a371d770985d016e6833560e1fa9429df39
Dan S <dspark0609@gmail.com>
2022-07-21 15:36:51 -0700

    Create button divs & js functions

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const screen = document.querySelector('calc-screen');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', getInput);
+});
+
+function getInput(e) {
+    let clickedButton = e.target.innerText;
+    outputScreen(clickedButton);
+}
+
+function outputScreen(userSelection) {
+    screen.innerText = userSelection;
+}
+
+
 function operate(operator, a, b) {
     let a = Number(a);
     let b = Number(b);
