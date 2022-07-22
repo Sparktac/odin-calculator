24e8b901045976679e62d7b9ea197b81348125b6
Dan S <dspark0609@gmail.com>
2022-07-22 15:06:15 -0700

    Work on script.js functions

--- script.js
+++ script.js
@@ -5,19 +5,49 @@ buttons.forEach(button => {
     button.addEventListener('click', getInput);
 });
 
-let lastValue;
+let lastValue = '';
+let firstSet = 0;
+let secondSet = 0;
+let savedOperator;
 
 function getInput(e) {
-    let clickedButton = e.target;
-    //console.log();
-    lastValue = clickedButton;
-    outputScreen(clickedButton);
-}
+    let clickedButton = e.target.getAttribute('data-key').toString();
+    //calculateInput(clickedButton);
+    if (isNaN(lastValue)) {
+        savedOperator = lastValue;
+        lastValue = '';
+    }
+
+    if (isOperator(clickedButton) == false) {
+        lastValue += clickedButton;
+    } else {
+        if (savedOperator == null) {
+            firstSet = lastValue;
+        } else {
+            secondSet = lastValue;
+        }
+        lastValue = clickedButton;
+    }
+
+    if (isEquals(clickedButton) == true) {
+        let result = operate(savedOperator, firstSet, secondSet);
+        console.log(result);
+    }
+
+    console.log(firstSet, secondSet);
+    outputScreen(lastValue);
+};
 
 function outputScreen(userSelection) {
     screen.innerText = userSelection;
-}
+};
 
+
+function calculateInput(currentButton) {
+    console.log(lastValue);
+    console.log(currentButton);
+    
+};
 
 function operate(operator, a, b) {
     a = Number(a);
@@ -35,8 +65,19 @@ function operate(operator, a, b) {
         default:
           return null;
     };
-}
+};
 
+function isEquals(value) {
+    return value == '=';
+};
+
+function isOperator(value) {
+    return isNaN(value); 
+};
+
+// function clearScreen() {
+//     screen.innerText = '';
+// };
 // HELPER FUNCTIONS
 
 function add(a, b) {
